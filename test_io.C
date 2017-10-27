50551bece6f06cb909f744171f7e613559f2434e
Benjamin S. Kirk <benjamin.kirk@nasa.gov>
2017-10-27 14:39:20 -0500

    test_io point of departure

---
+++ test_io.C
@@ -0,0 +1,113 @@
+#include <random>
+#include <algorithm>
+#include <iterator>
+#include <iostream>
+#include <functional>
+#include <iostream>
+#include <vector>
+#include <rpc/rpc.h>
+#include <cstddef>
+#include <stdio.h>
+#include <numeric>
+#include <boost/timer.hpp>
+#include <mpi.h>
+
+
+namespace
+{
+  static const std::size_t BUFSIZE = 2e8;
+  int rank, nprocs;
+  std::string basename = "test.";
+}
+
+
+
+template <typename T>
+void init_vector (std::vector<T> &data)
+{
+  boost::timer t;
+  data.resize(BUFSIZE);
+
+  std::iota(data.begin(), data.end(), static_cast<T>(rank));
+
+  std::cout << "Proc " << rank << " "
+            << "Initialized "
+            << BUFSIZE
+            << " values in "
+            << t.elapsed()
+            << " seconds\n";
+
+}
+
+
+
+void write_xdr (std::vector<double> &data)
+{
+  boost::timer t;
+
+  XDR *xdrs = new XDR;
+  std::string fname = basename + ".xdr";
+  FILE  *fp = fopen(fname.c_str(), "w");
+
+  xdrstdio_create (xdrs, fp, XDR_ENCODE);
+
+  xdr_vector(xdrs,
+	     (char*) &data[0],
+	     data.size(),
+	     sizeof(double),
+	     (xdrproc_t) xdr_double);
+
+  xdr_destroy (xdrs);
+  delete xdrs;
+
+  fflush (fp);
+  fclose (fp);
+
+  std::cout << "Proc " << rank << " "
+            << "XDR:\t Wrote " << data.size()*sizeof(double)/1.e6
+	    << "MB in " << t.elapsed() << " seconds\n";
+}
+
+
+
+void write_c (std::vector<double> &data)
+{
+  boost::timer t;
+
+  std::string fname = basename + ".bin";
+  FILE *fp = fopen(fname.c_str(), "w");
+
+  fwrite (data.empty() ? NULL : &data[0],
+          sizeof(double),
+          data.size(),
+          fp);
+
+  fflush (fp);
+  fclose (fp);
+
+  std::cout << "Proc " << rank << " "
+            << "C:\t Wrote " << data.size()*sizeof(double)/1.e6
+	    << "MB " << t.elapsed() << " seconds\n";
+}
+
+
+
+int main (int argc, char **argv)
+{
+  MPI_Init(&argc, &argv);
+  MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+  MPI_Comm_size(MPI_COMM_WORLD, &nprocs);
+
+  basename += std::string(std::to_string(rank));
+
+  {
+    std::vector<double> data; /**/ init_vector(data);
+
+    write_xdr(data);
+    write_c(data);
+  }
+
+  MPI_Finalize();
+
+  return 0;
+}
