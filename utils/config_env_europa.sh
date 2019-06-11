5e8f6003c3e3567ca5ce522ba8252aa936e4c401
Benjamin S. Kirk <benjamin.kirk@nasa.gov>
2019-06-11 09:33:22 -0500

    environment

---
+++ utils/config_env_europa.sh
@@ -0,0 +1,14 @@
+# Anaconda 3-5.0.1; python3
+module purge
+module load gcc slurm
+module load anaconda/3-5.0.1
+module load mpt
+module load mpi4py/3.0.1-python${PYTHON_VERSION}
+
+
+
+#####################
+echo "PYTHON_VERSION="${PYTHON_VERSION}
+echo $PYTHONPATH
+which mpiexec
+which python
