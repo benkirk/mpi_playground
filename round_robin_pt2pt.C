ea470cdaaee041b6e2983b257c19602aea5cce76
Benjamin Kirk <benjamin.s.kirk@gmail.com>
2023-05-12 17:19:41 -0600

    round robin. time ssend

--- round_robin_pt2pt.C
+++ round_robin_pt2pt.C
@@ -1,5 +1,6 @@
 #include "mpi.h"
 #include <vector>
+#include <sstream>
 #include <iostream>
 #include <iomanip>
 #include <assert.h>
@@ -9,36 +10,56 @@
 
 int main (int argc, char **argv)
 {
-  int nranks, myrank;
+  int nranks, myrank, mylocalrank;
+
   char hn[64], hns[4096*64];
-
-  gethostname(hn, sizeof(hn) / sizeof(char));
-  //std::cout << hn << std::endl;
-
-  MPI_Init (&argc, &argv);
-  MPI_Comm_size (MPI_COMM_WORLD, &nranks);
-  MPI_Comm_rank (MPI_COMM_WORLD, &myrank);
-
-  MPI_Allgather(hn,  64, MPI_CHAR,
-                hns, 64, MPI_CHAR,
-                MPI_COMM_WORLD);
-
-  if (0 == myrank)
-    for (unsigned int idx=0, cnt=0; cnt<nranks; cnt++, idx+=64)
-      {
-        for (unsigned int c=0; c<64 && hns[idx+c]!='\0'; c++)
-          std::cout << hns[idx+c];
-        std::cout << std::endl;
-      }
 
   const std::size_t
     bufcnt =  1000*1000*8,
     bufsize = bufcnt*(sizeof(unsigned int)),
     nrep = 10;
 
+  MPI_Init (&argc, &argv);
+  MPI_Comm_size (MPI_COMM_WORLD, &nranks);
+  MPI_Comm_rank (MPI_COMM_WORLD, &myrank);
+
+  {
+    MPI_Comm shmcomm;
+    MPI_Comm_split_type(MPI_COMM_WORLD, MPI_COMM_TYPE_SHARED, 0,
+                        MPI_INFO_NULL, &shmcomm);
+    MPI_Comm_rank(shmcomm, &mylocalrank);
+
+    gethostname(hn, sizeof(hn) / sizeof(char));
+
+    std::ostringstream oss;
+    oss << myrank << ":" << hn << ":" << mylocalrank;
+    std::string str = oss.str();
+
+    MPI_Allgather(&str[0],  64, MPI_CHAR,
+                  hns, 64, MPI_CHAR,
+                  MPI_COMM_WORLD);
+  }
+
   if (0 == myrank)
-    std::cout << "bufcnt  = " << bufcnt << std::endl
-              << "bufsize = " << bufsize << std::endl;
+    {
+      time_t now = time(0);
+
+      std::cout << "# --> BEGIN execution\n"
+                << "# " << ctime(&now)
+                << "# " << argv[0] << "\n"
+                << "# nranks = " << nranks << "\n";
+
+      for (unsigned int idx=0, cnt=0; cnt<nranks; cnt++, idx+=64)
+        {
+          std::cout << std::string(&hns[idx]);
+
+          (cnt != (nranks-1)) ? std::cout << ", " : std::cout << "\n";
+        }
+
+      std::cout << "# bufcnt  = " << bufcnt  << " elements\n"
+                << "# bufsize = " << bufsize << " (bytes)\n"
+                << "# myrank, procup, procdn=\n";
+    }
 
   std::vector<unsigned int> sbuf, rbuf(bufcnt,0);
   sbuf.reserve(bufcnt);
@@ -46,8 +67,6 @@ int main (int argc, char **argv)
     sbuf.push_back(i);
 
   std::vector<double>
-    //up(nranks,0.), allup(nranks*nranks,0.),
-    //dn(nranks,0.), alldn(nranks*nranks,0.),
     snd(nranks,0.), allsnd(nranks*nranks,0.);
 
   for (unsigned int rc=0; rc<nranks; rc++)
@@ -63,45 +82,15 @@ int main (int argc, char **argv)
         {
           MPI_Request rreq;
 
-          MPI_Irecv(&rbuf[0],  bufcnt, MPI_UNSIGNED, procdn, /* tag = */ 200, MPI_COMM_WORLD, &rreq);
+          MPI_Irecv(&rbuf[0],  bufcnt, MPI_UNSIGNED, procdn, /* tag = */ 1*nranks + step, MPI_COMM_WORLD, &rreq);
 
           MPI_Barrier(MPI_COMM_WORLD);
-
-          // // procup
-          // {
-          //   const double starttime = MPI_Wtime();
-
-          //   MPI_Sendrecv (&sbuf[0], bufcnt, MPI_UNSIGNED, procup, /* sendtag = */ 99,
-          //                 &rbuf[0], bufcnt, MPI_UNSIGNED, procdn, /* recvtag = */ 99,
-          //                 MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-
-          //   dn[procdn] += (MPI_Wtime() - starttime);
-
-          //   // check correctness
-          //   for (unsigned int i=procdn, j=0; j<bufcnt; i++, j++)
-          //     assert(rbuf[j] == i);
-          // }
-
-          // // procdn
-          // {
-          //   const double starttime = MPI_Wtime();
-
-          //   MPI_Sendrecv (&sbuf[0], bufcnt, MPI_UNSIGNED, procdn, /* sendtag = */ 100,
-          //                 &rbuf[0], bufcnt, MPI_UNSIGNED, procup, /* recvtag = */ 100,
-          //                 MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-
-          //   up[procup] += (MPI_Wtime() - starttime);
-
-          //   // check correctness
-          //   for (unsigned int i=procup, j=0; j<bufcnt; i++, j++)
-          //     assert(rbuf[j] == i);
-          // }
 
           // overlap Irecv / Ssend
           {
             const double starttime = MPI_Wtime();
 
-            MPI_Ssend(&sbuf[0], bufcnt, MPI_UNSIGNED, procup, /* tag = */ 200, MPI_COMM_WORLD);
+            MPI_Ssend(&sbuf[0], bufcnt, MPI_UNSIGNED, procup, /* tag = */ 1*nranks + step, MPI_COMM_WORLD);
 
             snd[procup] += (MPI_Wtime() - starttime);
 
@@ -113,61 +102,49 @@ int main (int argc, char **argv)
           }
         } // end avg loop over nrep
 
-      //dn[procdn] /= static_cast<double>(nrep);
-      //up[procup] /= static_cast<double>(nrep);
+      // compute average over nrep
       snd[procup] /= static_cast<double>(nrep);
 
-      // if (0 == myrank)
-      //   std::cout << "myrank, procup, procdn="
-      //             << myrank
-      //             << ", " << procup
-      //             << ", " << procdn
-      //             << " / " <<  dn[procdn] << " " << up[procup] << " " << snd[procup] << " (sec)"
-      //             << " / " << static_cast<double>(bufsize) / dn[procdn] << " " << static_cast<double>(bufsize) / up[procup] << " " << static_cast<double>(bufsize) / snd[procup] << " (b/sec)"
-      //             << std::endl;
-
       if (0 == myrank)
-        std::cout << "myrank, procup, procdn="
-                  << std::setw(3) << myrank
-                  << ", " << std::setw(3) << procup
-                  << ", " << std::setw(3) << procdn
+        std::cout << "# "
+                  << std::setw(5) << myrank
+                  << ", " << std::setw(5) << procup
+                  << ", " << std::setw(5) << procdn
                   << " / " << std::setw(12) << snd[procup] << "\t (sec)"
                   << " / " << std::setw(12) << static_cast<double>(bufsize) / snd[procup] << " (b/sec)"
                   << std::endl;
     }
 
-  // MPI_Allgather(&up[0],    nranks, MPI_DOUBLE,
-  //               &allup[0], nranks, MPI_DOUBLE,
-  //               MPI_COMM_WORLD);
+  {
+    const double starttime = MPI_Wtime();
 
-  // MPI_Allgather(&dn[0],    nranks, MPI_DOUBLE,
-  //               &alldn[0], nranks, MPI_DOUBLE,
-  //               MPI_COMM_WORLD);
+    MPI_Allgather(&snd[0],    nranks, MPI_DOUBLE,
+                  &allsnd[0], nranks, MPI_DOUBLE,
+                  MPI_COMM_WORLD);
 
-  MPI_Allgather(&snd[0],    nranks, MPI_DOUBLE,
-                &allsnd[0], nranks, MPI_DOUBLE,
-                MPI_COMM_WORLD);
+    const double elapsed = (MPI_Wtime() - starttime);
 
-  if (0 == myrank)
-    {
-      // for (unsigned int i=0; i<nranks; ++i)
-      //   {
-      //     for (unsigned int j=0; j<nranks; ++j)
-      //       std::cout << alldn[j*nranks + i] << " ";
+    if (0 == myrank)
+      {
+        std::cout << "# MPI_Allgather() on "
+                  << nranks << " ranks required " << elapsed << " (sec)\n";
 
-      //     std::cout << std::endl;
-      //   }
-      // std::cout << std::endl;
-      for (unsigned int i=0; i<nranks; ++i)
-        {
-          for (unsigned int j=0; j<nranks; ++j)
-            std::cout << allsnd[j*nranks + i] << " ";
+        for (unsigned int i=0; i<nranks; ++i)
+          {
+            std::cout << std::string(&hns[i*64]) << ", ";
+            for (unsigned int j=0; j<nranks; ++j)
+              {
+                std::cout << std::setprecision(8) << allsnd[j*nranks + i];
+                (j != (nranks-1)) ? std::cout << ", " : std::cout << "\n";
+              }
+          }
 
-          std::cout << std::endl;
-        }
-    }
+        std::cout << "# --> END execution" << std::endl;
+      }
+  }
 
   MPI_Finalize();
+
 
   return 0;
 }
